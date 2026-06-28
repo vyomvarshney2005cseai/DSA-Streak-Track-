d7dc1372f07b502aa70fb65f7a94ce01dad76ab0
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-06-28 23:10:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (21.21%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.c
@@ -0,0 +1,11 @@
+int findClosest(int x, int y, int z) {
+    if(abs(x-z)<abs(y-z)){
+        return 1;
+    }
+    else if((abs(x-z)>abs(y-z))){
+        return 2;
+    }
+    else{
+        return 0;
+    }
+}
